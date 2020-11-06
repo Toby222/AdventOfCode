8384c6406df687b69c170646d902f4d36cfff8c0
Tobias Berger <tobi.berger13@gmail.com>
2020-11-06 16:27:11 +0100

    Rust template

---
+++ rust/src/day00/part_1.rs
@@ -0,0 +1,24 @@
+pub(crate) fn part_1(_input: &'static str) -> u64 {
+    todo!("Part 1")
+}
+
+#[cfg(test)]
+mod tests {
+    const SAMPLE_INPUT: &str = include_str!("sample_input.txt");
+
+    #[test]
+    fn test_with_sample_solution() {
+        assert_eq!(
+            super::part_1(SAMPLE_INPUT),
+            todo!("Add result from example part 1")
+        )
+    }
+
+    #[test]
+    fn test_with_solution() {
+        assert_eq!(
+            super::part_1(crate::INPUT),
+            todo!("Add result for solved part 1")
+        );
+    }
+}
