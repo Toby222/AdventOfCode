26dc39e8155d82aa18fbec446a73a90cafc140a8
Tobias Berger <tobi.berger13@gmail.com>
2020-12-01 18:12:30 +0100

    JavaScript Template

---
+++ JS/template/solution_b.mjs
@@ -0,0 +1,15 @@
+import fs from "fs";
+
+/**
+ * Main function for Puzzle B
+ *
+ * @param {string} data - Puzzle input as a single string.
+ */
+async function main(data) {
+  return;
+}
+
+fs.readFile("input", (err, data) => {
+  if (err) throw err;
+  main(data.toString()).then(console.log).catch(console.error);
+});
