5b87de165bb83cf6fb12a1fd43ed1ad2288967ca
Tobias Berger <toby@tobot.dev>
2023-12-06 09:39:31 +0100

    Rust Day 6 Part 2

--- rust/src/day06/part_2.rs
+++ rust/src/day06/part_2.rs
@@ -1,5 +1,41 @@
-pub(crate) fn part_2(_input: &'static str) -> u64 {
-    todo!("Part 2")
+fn get_optimal_hold_time(maximum_time: u64, distance_to_beat: u64) -> (u64, u64) {
+    let mut minimum_hold_time = None;
+    let mut maximum_hold_time = None;
+    for time in 0..=maximum_time {
+        let distance = time * (maximum_time - time);
+        if distance > distance_to_beat && minimum_hold_time.is_none() {
+            minimum_hold_time = Some(time);
+            maximum_hold_time = Some(maximum_time - time);
+            break;
+        }
+    }
+    match (minimum_hold_time, maximum_hold_time) {
+        (Some(min), Some(max)) => (min, max),
+        _ => unreachable!("Should get a maximum and minimum hold time"),
+    }
+}
+
+pub(crate) fn part_2(input: &'static str) -> u64 {
+    let mut lines = input.lines();
+    let time = lines
+        .next()
+        .unwrap()
+        .split_ascii_whitespace()
+        .skip(1)
+        .collect::<String>()
+        .parse()
+        .expect("Invalid number");
+    let distance = lines
+        .next()
+        .unwrap()
+        .split_ascii_whitespace()
+        .skip(1)
+        .collect::<String>()
+        .parse()
+        .expect("Invalid number");
+
+    let (minimum_time, maximum_time) = get_optimal_hold_time(time, distance);
+    maximum_time - minimum_time + 1
 }
 
 #[cfg(test)]
@@ -8,17 +44,11 @@ mod tests {
 
     #[test]
     fn test_with_sample_solution() {
-        assert_eq!(
-            super::part_2(SAMPLE_INPUT),
-            todo!("Add result from example part 2")
-        );
+        assert_eq!(super::part_2(SAMPLE_INPUT), 71503);
     }
 
     #[test]
     fn test_with_solution() {
-        assert_eq!(
-            super::part_2(crate::INPUT),
-            todo!("Add result for solved part 2")
-        );
+        assert_eq!(super::part_2(crate::INPUT), 36872656);
     }
 }
